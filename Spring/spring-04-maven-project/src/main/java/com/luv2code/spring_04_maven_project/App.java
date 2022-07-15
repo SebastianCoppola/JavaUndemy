f09b3b02e5c68f7a6f2455691781703c7ae9d573
SebastianCoppola <seba.coppola@hotmail.com>
2022-07-15 18:59:27 -0300

    update with errors

---
+++ Spring/spring-04-maven-project/src/main/java/com/luv2code/spring_04_maven_project/App.java
@@ -0,0 +1,13 @@
+package com.luv2code.spring_04_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of Maven!" );
+    }
+}
---
+++ Spring/spring-04-maven-webproject/src/main/java/com/luv2code/springdemo/mvc/HomeController.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	//controller method
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String showPage() {
+		return "home";
+	}
+}
